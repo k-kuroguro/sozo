33bb5283f1af5b9081b687c1dbdb1a9a4cb97bec
k-kuroguro <lingjiujinshan@gmail.com>
2024-11-24 15:02:09 +0900

    feat: Add unique constraint and index

--- apps/web/schemas.py
+++ apps/web/schemas.py
@@ -7,7 +7,7 @@ class AggregatedConcentrationStatus(SQLModel, table=True):
     __tablename__ = "aggregated_concentration_status"
 
     id: int | None = Field(default=None, primary_key=True)
-    start_time: datetime
-    end_time: datetime
+    start_time: datetime = Field(index=True, unique=True)
+    end_time: datetime = Field(index=True, unique=True)
     overall_score: float
     sleeping_confidence: float
